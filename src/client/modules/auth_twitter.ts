3a78874ceac657a1d0c6ef8d857cdb6418a499a9
RepleDEV <ragazzochaesa@gmail.com>
2022-06-25 18:23:13 +0700

    Fix subtleCrypto error

--- src/client/modules/auth_twitter.ts
+++ src/client/modules/auth_twitter.ts
@@ -39,25 +39,25 @@ interface AuthURLOptions {
     scope: AuthURLScopes[];
     state: string;
 }
-class AuthURL {
-    clientId: string;
-    constructor(clientId: string) {
-        this.clientId = clientId;
-    }
-    generate(options: AuthURLOptions): { url: string, verifier: string } {
-        let url = "https://twitter.com/i/oauth2/authorize?";
-        url += "response_type=code&";
-        url += `client_id=${this.clientId}&`;
-        url += `redirect_uri=${options.redirect_uri}&`;
-        url += `scope=${options.scope.join(" ")}&`;
-        url += `state=${options.state}&`;
-        const codeChallenge = new CodeChallenge(56, true);
-        codeChallenge.generate();
-        url += `code_challenge=${codeChallenge.hash}&`;
-        url += "code_challenge_method=S256";
+// class AuthURL {
+//     clientId: string;
+//     constructor(clientId: string) {
+//         this.clientId = clientId;
+//     }
+//     generate(options: AuthURLOptions): { url: string, verifier: string } {
+//         let url = "https://twitter.com/i/oauth2/authorize?";
+//         url += "response_type=code&";
+//         url += `client_id=${this.clientId}&`;
+//         url += `redirect_uri=${options.redirect_uri}&`;
+//         url += `scope=${options.scope.join(" ")}&`;
+//         url += `state=${options.state}&`;
+//         const codeChallenge = new CodeChallenge(56, true);
+//         codeChallenge.generate();
+//         url += `code_challenge=${codeChallenge.hash}&`;
+//         url += "code_challenge_method=S256";
 
-        url = encodeURI(url);
+//         url = encodeURI(url);
 
-        return { url, verifier: codeChallenge.verifier };
-    }
-}
+//         return { url, verifier: codeChallenge.verifier };
+//     }
+// }
