9985f86ffdea26d5e5e1d432d60adee9ad83c465
RepleDEV <ragazzochaesa@gmail.com>
2022-06-28 08:40:49 +0700

    Should've tested locally before committing, novice mistake. Anyway fixed parsing

--- src/server/modules/jawsGetAuth.ts
+++ src/server/modules/jawsGetAuth.ts
@@ -1,12 +1,11 @@
 // I MAY BE GETTING THIS WRONG
 
 export default function jawsGetAuth() {
-    let JAWSDB_MARIA_URL = process.env.JAWSDB_MARIA_URL as string;
+    const JAWSDB_MARIA_URL = process.env.JAWSDB_MARIA_URL as string;
 
-    JAWSDB_MARIA_URL = JAWSDB_MARIA_URL.substring("mysql://".length);
-    JAWSDB_MARIA_URL.split("/");
-    const default_schema = JAWSDB_MARIA_URL[1];
-    const auth = JAWSDB_MARIA_URL[1].split("@");
+    const split_url = JAWSDB_MARIA_URL.substring("mysql://".length).split("/");
+    const default_schema = split_url[1];
+    const auth = split_url[0].split("@");
     const [username, password] = auth[0].split(":");
     const [hostname, port] = auth[1].split(":");
 
