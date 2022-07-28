3ea2e648b5d0c3b84c8d80201ee5790950e8ffa9
RepleDEV <ragazzochaesa@gmail.com>
2022-07-28 14:36:15 +0700

    Moved random string generator

--- __tests__/server/modules/jawsGetAuth.test.ts
+++ __tests__/server/modules/jawsGetAuth.test.ts
@@ -1,16 +1,5 @@
 import jawsGetAuth from "../../../src/server/modules/jawsGetAuth";
-
-function randomStringGenerator(length = 16, charset?: string) {
-    const _charset = "abcdefghijklmnopqrstuvwxyz0123456789" || charset;
-
-    let res = "";
-    for (let i = 0;i < length;i++) {
-        const randIndex = Math.floor(Math.random() * _charset.length);
-        res += _charset[randIndex];
-    }
-
-    return res;
-}
+import randomStringGenerator from "../../modules/randomStringGenerator";
 
 describe("Jaws get auth", () => {
     describe("Simple auth", () => {
