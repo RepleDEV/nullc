f08afa4bda224f7bd9f53b2399f0913677cf978b
RepleDEV <ragazzochaesa@gmail.com>
2022-08-13 19:44:15 +0700

    New home page

---
+++ src/client/pages/Home_2.0.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+// import "../scss/pages/Home";
+
+class Home extends Component {
+    render(): React.ReactNode {
+        return (
+            <div className="page Home">
+                
+            </div>
+        );
+    }
+}
+
+export default Home;
