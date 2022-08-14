3befad91fb6fdefc71aed4985c23eaec5badd9fe
RepleDEV <ragazzochaesa@gmail.com>
2022-08-14 07:32:20 +0700

    Home page

--- src/client/pages/Home_2.0.tsx
+++ src/client/pages/Home_2.0.tsx
@@ -1,12 +1,29 @@
 import React, { Component } from "react";
 
-// import "../scss/pages/Home";
+import "../scss/pages/Home";
 
 class Home extends Component {
     render(): React.ReactNode {
         return (
             <div className="page Home">
-                
+                <div className="twin-columns">
+                    <div className="column left">
+                        <div className="name-card">
+                            <div className="profile-picture-container">
+                                <img
+                                    src="https://cdn.discordapp.com/avatars/246789625715228672/8441bcb570405a341717dead19458801.png?size=4096"
+                                    alt="Profile Image" 
+                                />
+                            </div>
+                            <span className="name">
+                                null
+                            </span>
+                        </div>
+                    </div>
+                    <div className="column right">
+
+                    </div>
+                </div>
             </div>
         );
     }
