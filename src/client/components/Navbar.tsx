e0c735a4636ef9c1f0c21e162186197ab387b51f
RepleDEV <ragazzochaesa@gmail.com>
2022-09-02 08:39:02 +0700

    Mobile navbar/sidebar

--- src/client/components/Navbar.tsx
+++ src/client/components/Navbar.tsx
@@ -2,27 +2,51 @@ import React, { Component } from "react";
 import { Link, To } from "react-router-dom";
 
 import "../scss/Navbar";
+import "../scss/Sidebar";
 import { EmptyComponentState } from "../types/Component";
 import { Navbar as NavbarTypes } from "../types/Components";
 import Popup from "./Popup";
+import Bars from "./svg/Bars";
 import Heart from "./svg/Heart";
 import Twitter from "./svg/Twitter";
 
-class NavLink extends Component<{to: To}> {
+class NavLink extends Component<{to: To, onClick?: () => void}> {
 	render(): React.ReactNode {
 		return (
-			<div className="nav-link">
+			<div className="nav-link" onClick={this.props.onClick}>
 				<Link to={this.props.to}>{this.props.children}</Link>
 			</div>
 		);
 	}
 }
 
-class Navbar extends Component<NavbarTypes.NavbarProps, EmptyComponentState> {
+function Links({ onClick }: { onClick?: () => void }) {
+	// TODO: Wack onclick workaround pls fix
+	return (
+		<>
+			<NavLink to="/" onClick={onClick}>home</NavLink>
+			<NavLink to="/repo" onClick={onClick}>tweet repo</NavLink>
+			<NavLink to="/mail" onClick={onClick}>mail</NavLink>
+		</>
+	);
+}
+
+class Navbar extends Component<NavbarTypes.NavbarProps, {
+	showSidebar: boolean;
+}> {
 	constructor(props: NavbarTypes.NavbarProps) {
 		super(props);
 
+		this.state = {
+			showSidebar: false,
+		};
+
+		this.toggleSidebar = this.toggleSidebar.bind(this);
 		this.handleLogin = this.handleLogin.bind(this);
+	}
+
+	toggleSidebar() {
+		this.setState({ showSidebar: !this.state.showSidebar });
 	}
 
 	handleLogin() {
@@ -70,12 +94,21 @@ class Navbar extends Component<NavbarTypes.NavbarProps, EmptyComponentState> {
 		}
 
 		return (
-			<div className="navbar">
-				<NavLink to="/">home</NavLink>
-				<NavLink to="/repo">tweet repo</NavLink>
-				<NavLink to="/mail">mail</NavLink>
-				<div className="toolbar-container">{element}</div>
-			</div>
+			<>
+				<div className={`mobile-sidebar ${this.state.showSidebar && "shown"}`}>
+					<div className="content">
+						<Links onClick={this.toggleSidebar}/>
+					</div>
+					<div className="overlay-background" onClick={this.toggleSidebar}/>
+				</div>
+				<div className="navbar">
+					<div className="bars-container" onClick={this.toggleSidebar}>
+						<Bars/>
+					</div>
+					<Links/>
+					<div className="toolbar-container">{element}</div>
+				</div>
+			</>
 		);
 	}
 }
---
+++ src/client/components/svg/Bars.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Bars() {
+    return (
+            // Generator: Adobe Illustrator 24.0.0, SVG Export Plug-In
+            <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="42px"
+                height="36px" viewBox="0 0 42 36" xmlSpace="preserve">
+                <defs>
+                </defs>
+                <g>
+                    <path d="M39,6H3C1.3,6,0,4.7,0,3v0c0-1.7,1.3-3,3-3h36c1.7,0,3,1.3,3,3v0C42,4.7,40.7,6,39,6z"/>
+                </g>
+                <g>
+                    <path d="M39,36H3c-1.7,0-3-1.3-3-3v0c0-1.7,1.3-3,3-3h36c1.7,0,3,1.3,3,3v0C42,34.7,40.7,36,39,36z"/>
+                </g>
+                <g>
+                    <path d="M39,20H3c-1.7,0-3-1.3-3-3v0c0-1.7,1.3-3,3-3h36c1.7,0,3,1.3,3,3v0C42,18.7,40.7,20,39,20z"/>
+                </g>
+            </svg>
+        );
+}
+
+export default Bars;
