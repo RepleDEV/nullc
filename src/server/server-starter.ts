f612badf4e7a8c063047420fe390478966a85e46
RepleDEV <ragazzochaesa@gmail.com>
2022-10-05 20:26:17 +0700

    Fix moots.ts error

--- src/server/server-starter.ts
+++ src/server/server-starter.ts
@@ -1,4 +1,3 @@
-import { refreshMootsList } from "./scripts/moots";
 import app, { redisClient, mailDB } from "./server";
 
 const port = process.env.PORT || 3000;
@@ -6,7 +5,6 @@ const port = process.env.PORT || 3000;
 (async () => {
 	await mailDB.setup(process.env.NODE_ENV === "production");
 
-	if (process.env.NODE_ENV === "production") await refreshMootsList();
 	await redisClient.connect();
 
 	app.listen(port, () => console.log(`Server listening on port: ${port}`));
